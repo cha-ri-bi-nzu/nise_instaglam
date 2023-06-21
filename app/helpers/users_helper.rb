ee09817013235479b846e506ec176851fecb6879
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-22 00:10:40 +0900

    プロフィール編集画面作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def current_user_password?(password)
+    if password == current_user.password
+      <%= render 'form', user: @user %>
+    else
+      redirect_to edit_user_path, notice: "The password is in correct."
+    end
+  end
 end

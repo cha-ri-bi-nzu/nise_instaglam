a07efc55b947f507cf05615d4c93d6072e87f22a
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-23 03:16:20 +0900

    他ユーザーからのプロフィール編集の禁止機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :are_you_current_user?, only: [:edit]
+
   def new
     @user = User.new
   end
@@ -21,17 +24,29 @@ class UsersController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "Editing done"
+    else
+      render :edit
+    end
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image, :profile_image_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def are_you_current_user?
+    redirect_to user_path(@user.id) unless @user.id == current_user.id
   end
 end

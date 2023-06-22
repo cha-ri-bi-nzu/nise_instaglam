d4810a6f7e7d6d3f6ce05c047cb1d0a84b129f2e
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-23 03:15:00 +0900

    不要なメソッドの削除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,4 @@
 module UsersHelper
-  def current_user_password?(password)
-    if password == current_user.password
-      true
-    else
-      false
-    end
-  end
-
   def choose_new_or_edit
     if action_name == 'new'
       confirm_users_path

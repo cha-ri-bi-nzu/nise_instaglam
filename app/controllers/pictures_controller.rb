99174b157abeb28c7faf73ddcb07183d54b137c2
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-24 22:58:50 +0900

    投稿編集機能追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only [:show, :edit, :update, :destroy]
   def index
     @pictures = Picture.all
   end
@@ -29,6 +30,11 @@ class PicturesController < ApplicationController
   end
 
   def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "You could edit now🙆"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -37,5 +43,9 @@ class PicturesController < ApplicationController
   private
   def picture_params
     params.require(:picture).permit(:image, :image_cache, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end

78d35889e6ed7009fce96ddb2840e4b7ad3f7ea5
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-26 23:52:50 +0900

    実装できなかったコードの削除

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,11 +1,2 @@
 module FavoritesHelper
-  # def here_page
-  #   if request.url&.include?('/pictures/index')
-  #     pictures_path
-  #   elsif request.url&.include?('/pictures/show')
-  #     picture_path
-  #   elsif request.url&.include?('/favorites/index')
-  #     favorites_path
-  #   end
-  # end
 end

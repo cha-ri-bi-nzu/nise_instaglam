5d6833fbb10c29a6811ba24559252c867ab7a96a
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-26 13:44:52 +0900

    各ページのリンク作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -31,6 +31,7 @@ class PicturesController < ApplicationController
 
   def show
     @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+    @url = request.referer
   end
 
   def edit
